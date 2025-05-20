1f0cb51b3c29c91d3d573511a7018c203e037d7b
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-20 17:14:41 +0200

    [FIX] correzioni su chiamata ajax

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -7,21 +7,29 @@ const MainNav = () => {
     const [movies, setMovies] = useState([])
 
     const handleSearch = () => {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=0450842658618b969d5d58f51db357a6&query=${search}`).then((resp) => {
-            setMovies(resp.data)
-        })
-    }
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=0450842658618b969d5d58f51db357a6&query=${search}`)
+            .then((resp) => {
+                console.log(resp.data.results);
+                setMovies(resp.data.results);
+            })
+            .catch((err) => {
+                console.error("Errore nella chiamata:", err);
+            });
+    };
 
     return (
 
+        <>
+            <header>
+                <div className="input-group mb-3">
+                    <button onClick={handleSearch} className="btn btn-outline-secondary" type="button" id="button-addon1">cerca</button>
+                    <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" className="form-control" placeholder="Inserisci il film" />
+                </div>
+            </header>
 
-        <header>
-            <div className="input-group mb-3">
-                <button onClick={handleSearch} className="btn btn-outline-secondary" type="button" id="button-addon1">cerca</button>
-                <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" className="form-control" placeholder="Inserisci il film" />
-            </div>
-        </header>
+        </>
     )
+
 
 }
 

36d755ea3dfd793c789bf3b49a33ee4873c2fdd1
mauroanastasi <mauroanastasi1@gmail.com>
2025-05-20 16:38:57 +0200

    [UPD] definisco chiamata ajax

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const MainNav = () => {
     const [search, setSearch] = useState("")
+
+    const handleSearch = () => {
+        axios.get("https://api.themoviedb.org/3/search/movie?api_key=0450842658618b969d5d58f51db357a6&query=${search}").then((resp) => {
+            setSearch(resp.data)
+        })
+    }
+
     return (
+
 
         <header>
             <div className="input-group mb-3">
-                <button className="btn btn-outline-secondary" type="button" id="button-addon1">cerca</button>
+                <button onClick={handleSearch} className="btn btn-outline-secondary" type="button" id="button-addon1">cerca</button>
                 <input value={search} onChange={(e) => setSearch(e.target.value)} type="text" className="form-control" placeholder="Inserisci il film" />
             </div>
         </header>
